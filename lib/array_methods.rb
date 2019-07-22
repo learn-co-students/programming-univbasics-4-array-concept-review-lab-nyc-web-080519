52a79875e63d5bc851562537a362de7ca5234765
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-22 07:29:54 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  length = array.length
+  
+  length.times do |index|
+    if array[index] == value_to_find
+      return index
+    end
+  end
+  
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  sorted = array.sort
+  return sorted[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  sorted = array.sort
+  return sorted[0]
 end
